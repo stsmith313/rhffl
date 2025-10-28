122ec58d8d8c1e79a7af982c72aec3d759d57fab
Sam Smith <stsmith313@gmail.com>
2025-10-28 09:06:09 -0400

    Week 8

--- JS/matchups.js
+++ JS/matchups.js
@@ -17,7 +17,7 @@ const LOCAL_LOGOS = {
   15: "Logos/chris.png",
 };
 
-let currentWeek = 8; // <-- Force starting week here
+let currentWeek = 9; // <-- Force starting week here
 
 // --- Fetch and render matchups ---
 async function fetchMatchups(leagueId, seasonId, week) {
