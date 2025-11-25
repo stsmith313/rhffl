f7f5d5778c2736df1d2c63a717a29c41d0d5bfb6
Sam Smith <stsmith313@gmail.com>
2025-11-25 08:53:00 -0500

    Weekly update

--- JS/matchups.js
+++ JS/matchups.js
@@ -17,7 +17,7 @@ const LOCAL_LOGOS = {
   15: "Logos/chris.png",
 };
 
-let currentWeek = 12; // <-- Force starting week here
+let currentWeek = 13; // <-- Force starting week here
 
 // --- Fetch and render matchups ---
 async function fetchMatchups(leagueId, seasonId, week) {
@@ -123,7 +123,7 @@ async function loadMatchups() {
 async function changeWeek(delta) {
   currentWeek += delta;
   if (currentWeek < 1) currentWeek = 1;
-  if (currentWeek > 14) currentWeek = 14;
+  if (currentWeek > 14) currentWeek = 17;
   await loadMatchups();
 }
 
